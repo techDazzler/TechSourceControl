182281fed971a5fe7a5ba5d878994f2ba921e414
Manavpreet Kang <Mkang1106@conestogac.on.ca>
2016-09-14 12:19:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mkang1106/techsourcecontrol/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mkang1106.techsourcecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
